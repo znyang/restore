afa799a5de2e3f161738ec6df973699abfb014d5
znyang <xggcl@sina.com>
2016-02-02 00:06:37 +0800

    create projects

---
+++ app/src/main/java/com/zen/android/restore/demo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.zen.android.restore.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.zen.android.restore.R.layout.activity_main);
+    }
+}
---
+++ library/src/main/java/com/zen/android/restore/RestoreProcessor.java
@@ -0,0 +1,7 @@
+package com.zen.android.restore;
+
+/**
+ * Created by zheng on 2016/2/1.
+ */
+public class RestoreProcessor {
+}
